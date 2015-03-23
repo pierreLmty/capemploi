d1212ffde537609f200036b685c772538768bea9
pierrelmty <pierre.lemetayer@etu.univ-nantes.fr>
2015-03-23 14:07:18 +0100

    Ajout include du head

---
+++ offre1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php include_once 'html-includes/head.html'; ?>
+	</head>
+	
+	<body>
+		
+		<header>
+			<?php include_once 'html-includes/header.html'; ?>
+		</header>
+		
+		<nav>
+			<?php include_once 'html-includes/menu.html'; ?>
+		</nav>
+		
+		<footer>
+			<?php include_once 'html-includes/footer.html'; ?>
+		</footer>
+		
+	</body>
+</html>
--- offres-emploi.php
+++ offres-emploi.php
@@ -1,11 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>CAP EMPLOI 44 Nantes - Ressources Handicaps - Organismes de placement spécialisés au service des personnes handicapées et des employeurs pour l'adéquation de l'emploi, compétences et handicap</title>
-		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-		<script type="text/javascript" src="scripts/script.js"></script>
-    	<link rel="stylesheet" type="text/css" href="style/style.css"/>
-		<meta charset="utf-8"/>
+		<?php include_once 'html-includes/head.html'; ?>
 	</head>
 	
 	<body>
@@ -28,7 +24,7 @@
 						<tr><th>Intitulé du poste</th><th>N° de l'offre</th><th>Contrat</th><th>Rythme</th><th>Lieu de travail</th></tr>
 					</thead>
 					<tbody>
-						<tr onclick="window.location.href = 'offre1.php'"><td>Agent des services hospitaliers</td><td>7156A</td><td>CDD</td><td>Temps partiel</td><td>LE LOROUX BOTTEREAU</td></tr>
+						<tr onclick="alert('Détails de l\'offre...');"><td>Agent des services hospitaliers</td><td>7156A</td><td>CDD</td><td>Temps partiel</td><td>LE LOROUX BOTTEREAU</td></tr>
 						<tr onclick="alert('Détails de l\'offre...');"><td>Juriste conseiller clientèle H/F</td><td>7154A</td><td>CDD</td><td>Temps complet</td><td>ST HERBLAIN</td></tr>
 						<tr onclick="alert('Détails de l\'offre...');"><td>Opérateur logistique</td><td>356A FM</td><td>CDI</td><td>en équipe</td><td>MONTOIR DE BRETAGNE</td></tr>
 					</tbody>
