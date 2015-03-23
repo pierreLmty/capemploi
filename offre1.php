25de101abf7210e7f9e3e3cea47b5005334254f7
pierrelmty <pierre.lemetayer@etu.univ-nantes.fr>
2015-03-23 14:46:52 +0100

    Ajout d'une page d'offre

--- offre1.php
+++ offre1.php
@@ -14,6 +14,112 @@
 			<?php include_once 'html-includes/menu.html'; ?>
 		</nav>
 		
+		<section>
+			<article>
+				<h2 id="titre_offres_emploi">Offres Cap emploi réservées aux Travailleurs Handicapés</h2>
+				<h3><a href="offre-emploi.php">Retour à la liste</a></h3>
+				
+				<table id="tableau_offre">
+					<thead>
+						<tr>
+							<th colspan="2">OFFRE n° 7157P</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Intitulé du poste</td>
+							<td>agent de service</td>
+						</tr>
+						<tr>
+							<td>Code ROME</td>
+							<td>K2204 - Nettoyage de locaux</td>
+						</tr>
+						<tr>
+							<td>Code NAF</td>
+							<td>81.21Z</td>
+						</tr>
+						<tr>
+							<td>Offre à pourvoir le</td>
+							<td>23/03/2015</td>
+						</tr>
+						<tr>
+							<td>Type de contrat</td>
+							<td>CDD - 4 à 24 mois</td>
+						</tr>
+						<tr>
+							<td>Nature du contrat</td>
+							<td>Contrat de travail non aidé</td>
+						</tr>
+						<tr>
+							<td>Temps de travail</td>
+							<td>en équipe - de 20 à 35 heures suivant les postes - 6 heures-13 heures</td>
+						</tr>
+						<tr>
+							<td>Lieu de travail</td>
+							<td>NANTES</td>
+						</tr>
+						<tr>
+							<td>Moyen d'accès</td>
+							<td>Transports en commun</td>
+						</tr>
+						<tr>
+							<td>Descriptif du poste</td>
+							<td>entretien des bureaux(dépoussiérage-aspiration et nettoyage des sanitaires) et des cages d'escalier ainsi que des travaux de répurgation</td>
+						</tr>
+						<tr>
+							<td>Rémunération</td>
+							<td>9.86 euros brut de l'heure</td>
+						</tr>
+					</tbody>
+					<thead>
+						<tr>
+							<th colspan="2">Profil recherché</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Profil recherché</td>
+							<td>Capacité à prendre ses fonctions à 6 heures du matin -capacité à suivre un planning de travail dans un ordre précis (chantiers et tâches)et dans le temps prévu -Avoir résolu ses problèmes de garde d'enfants -Goût pour le travail en équipe -Aptitudes relationnelles -Capacité à suivre des consignes (maitrise du français à l'oral et à l'écrit notamment être en capacité de lire les modes d'emploi concernant les produits d'entretien) « il s’agit d’une offre récurrente de la part de l’employeur ;il a des besoins tout au long de l’année " Le permis est souhaité Il s'agit de postes physiques (répurgation,entretiens de cages d'escaliers)</td>
+						</tr>
+						<tr>
+							<td>Contre-indications</td>
+							<td><ul>
+								<li>Accès poste de travail : Monter/descendre escalier</li>
+								<li>Apt intell/cognitif : Comprendre consigne orale</li>
+								<li>Postures travail : Station debout/pietinement</li>
+								<li>Aptitudes et tolérance psy. : Travail en groupe</li>
+							</ul></td>
+						</tr>
+					</tbody>
+					<thead>
+						<tr>
+							<th colspan="2">Postuler</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr id="postuler">
+							<td colspan="2">
+								Pour répondre à cette offre,<br/>
+								merci d'adresser votre candidature à <b>Laure BRETIGNIERES.</b><br/>
+								<b>CV + Lettre de motivation</b><br/>
+								+ Votre titre de l'obligation d'emploi Handicap<br/>
+								pour les personnes non accompagnées à Cap emploi Nantes.<br/>
+								par e-mail à l'adresse :<br/>
+								l.bretignieres@capemploi44nantes.org<br/>
+								(envoi par email : fichiers au format PDF de préférence)<br/>
+								ou par courrier postal à l'adresse :<br/>
+								Cap emploi 44 Nantes<br/>
+								Mme Laure BRETIGNIERES - Offre 7157P<br/>
+								1 rue Didienne<br/>
+								BP 40816<br/>
+								44008 Nantes Cedex 1
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</article>
+		</section>
+		
 		<footer>
 			<?php include_once 'html-includes/footer.html'; ?>
 		</footer>
