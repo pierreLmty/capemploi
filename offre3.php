9a156dfd492afdfb553eaabe61a23dde5b1bb21d
pierrelmty <pierre.lemetayer@etu.univ-nantes.fr>
2015-03-24 14:01:47 +0100

    Ajout de l'offre 2 et 3

---
+++ offre3.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php include_once 'html-includes/head.html'; ?>
+	</head>
+	
+	<body>
+		
+		<header>
+			<?php include_once 'html-includes/header.html'; ?>
+		</header>
+		
+		<nav>
+			<?php include_once 'html-includes/menu.html'; ?>
+		</nav>
+		
+		<section>
+			<article>
+				<h2 id="titre_offres_emploi">Offres Cap emploi réservées aux Travailleurs Handicapés</h2>
+				<h3><a href="offre-emploi.php">Retour à la liste</a></h3>
+				
+				<table id="tableau_offre">
+					<thead>
+						<tr>
+							<th colspan="2">OFFRE n° 7160A</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Intitulé du poste</td>
+							<td>agent des services hospitaliers</td>
+						</tr>
+						<tr>
+							<td>Code ROME</td>
+							<td>J1301 - Personnel polyvalent des services hospitaliers</td>
+						</tr>
+						<tr>
+							<td>Code NAF</td>
+							<td>86.10Z</td>
+						</tr>
+						<tr>
+							<td>Offre à pourvoir le</td>
+							<td>07/04/2015</td>
+						</tr>
+						<tr>
+							<td>Type de contrat</td>
+							<td>CDD - 1 an</td>
+						</tr>
+						<tr>
+							<td>Nature du contrat</td>
+							<td>CAE</td>
+						</tr>
+						<tr>
+							<td>Temps de travail</td>
+							<td>Temps partiel - 20H/SEMAINE - 13H30 à 17H30 du lundi au vendredi</td>
+						</tr>
+						<tr>
+							<td>Lieu de travail</td>
+							<td>LE LOROUX BOTTEREAU</td>
+						</tr>
+						<tr>
+							<td>Moyen d'accès</td>
+							<td>Moyens personnels</td>
+						</tr>
+						<tr>
+							<td>Descriptif du poste</td>
+							<td><ul>
+								<li>Entretien des locaux collectifs </li>
+								<li>Vaisselle</li>
+								<li>Entretien de bureaux et vestiaires</li>
+								<li>Entretien des appareils ménagers, des chariots, des fauteuils</li>
+								<li>aide en hôtellerie</li>
+							</ul></td>
+						</tr>
+						<tr>
+							<td>Rémunération</td>
+							<td>SMIC sur 20H semaine</td>
+						</tr>
+					</tbody>
+					<thead>
+						<tr>
+							<th colspan="2">Profil recherché</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Expérience</td>
+							<td>1ère expérience</td>
+						</tr>
+						<tr>
+							<td>Profil recherché</td>
+							<td>Formation BEP sanitaire et social souhaitée la personne devra être rigoureuse, organisée, discrète, aimer le travail en équipe, savoir lire et écrire, avoir des qualités relationnelles</td>
+						</tr>
+						<tr>
+							<td>Contre-indications</td>
+							<td><ul>
+								<li>Apt intell/cognitif : Concentration et attention</li>
+								<li>Postures travail : Port de charges < 5 kg</li>
+								<li>Apt intell/cognitif : Comprendre consigne écrite</li>
+								<li>Aptitudes et tolérance psy : Travail en équipe</li>
+								<li>Postures travail : Station debout/pietinement</li>
+								<li>Aptitudes et tolérance psy : Initiative et responsabilité</li>
+								<li>Apt intell/cognitif : Comprendre consigne orale</li>
+							</ul></td>
+						</tr>
+					</tbody>
+					<thead>
+						<tr>
+							<th colspan="2">Postuler</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td colspan="2" id="postuler">
+								Pour répondre à cette offre,<br/>
+								merci d'adresser votre candidature à <b>Bérangère TERRIEN.</b><br/>
+								<b>CV + Lettre de motivation</b><br/>
+								+ Votre titre de l'obligation d'emploi Handicap<br/>
+								pour les personnes non accompagnées à Cap emploi Nantes.<br/>
+								par e-mail à l'adresse :<br/>
+								b.terrien@capemploi44nantes.org<br/>
+								(envoi par email : fichiers au format PDF de préférence)<br/>
+								ou par courrier postal à l'adresse :<br/>
+								Cap emploi 44 Nantes<br/>
+								Mme Laure BRETIGNIERES - Offre 7157P<br/>
+								1 rue Didienne<br/>
+								BP 40816<br/>
+								44008 Nantes Cedex 1
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</article>
+		</section>
+		
+		<footer>
+			<?php include_once 'html-includes/footer.html'; ?>
+		</footer>
+		
+	</body>
+</html>
